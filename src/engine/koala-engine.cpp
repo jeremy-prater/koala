9688a654679cc6057db1a3daa07290b1c4ebfdaf
Jeremy Prater <silverevo03@gmail.com>
2019-12-06 13:13:05 -0800

    - Fixed RapidJSON and added versioning

--- src/engine/koala-engine.cpp
+++ src/engine/koala-engine.cpp
@@ -1,9 +1,13 @@
 #include "koala-engine.hpp"
+#include "koala-version.hpp"
 
 KoalaEngine::KoalaEngine(const Arguments &arguments)
     : Platform::Application{arguments},
       logger("Main", DebugLogger::DebugColor::COLOR_WHITE, false) {
-  logger.WriteLog(DebugLogger::DebugLevel::DEBUG_INFO, "I'm alive!");
+  logger.WriteLog(DebugLogger::DebugLevel::DEBUG_INFO,
+                  "Koala Engine Startup V%d.%d [%s]",
+                  KOALA_ENGINE_VERSION_MAJOR, KOALA_ENGINE_VERSION_MINOR,
+                  KOALA_ENGINE_BUILD_SHA);
   /* TODO: Add your initialization code here */
 }
 
