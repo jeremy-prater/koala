d8540289a2d0b9863cf20bb63e96bcd6862048ee
Jeremy Prater <silverevo03@gmail.com>
2020-04-09 16:19:16 -0700

    - Update env script to use nproc / host_os

--- env_koala.sh
+++ env_koala.sh
@@ -8,7 +8,19 @@ echo "=========> Build : Destroying build"
 rm -rf build
 mkdir build
 
-echo "=========> Build : Generating new build"
+HOST_OS=`uname` 
+if [ $HOST_OS = 'Linux' ]
+then
+    NUM_CORES=`nproc`
+elif [ $HOST_PS = 'Darwin' ]
+then
+    NUM_CORES=`sysctl -n hw.logicalcpu`
+else
+    echo "Unknown OS type! $HOST_OS"
+    exit -1
+fi
+
+echo "=========> Build : Generating new build on $HOST_OS using $NUM_CORES cores"
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-cmake --build . -j 24
+cmake --build . -j $NUM_CORES
