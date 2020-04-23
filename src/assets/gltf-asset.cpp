b8b494a126b37ca66c2d9bc500bf34fc5d3199a0
Jeremy Prater <silverevo03@gmail.com>
2020-04-22 22:57:46 -0700

    - GLTF is getting closer...

--- src/assets/gltf-asset.cpp
+++ src/assets/gltf-asset.cpp
@@ -30,8 +30,8 @@ void GLTFAsset::BuildChildTree(const std::string &path,
 
   auto objectNode = gltfImporter.object3D(parentNode);
   const std::string objectName =
-      gltfImporter.object3DName(objectNode->instance());
-  logger.Info("Parsing scene : [%s/%s]", path.c_str(), objectName.c_str());
+      path + "/" + gltfImporter.object3DName(objectNode->instance());
+  logger.Info("Parsing scene : [%s]", objectName.c_str());
 
   auto childNodes = objectNode->children();
   for (auto currentNode : childNodes) {
@@ -52,26 +52,29 @@ void GLTFAsset::BuildChildTree(const std::string &path,
   for (uint32_t sceneID = 0; sceneID < sceneCount; sceneID++) {
     const std::string sceneName = gltfImporter.sceneName(sceneID);
     Magnum::Trade::SceneData sceneData = *gltfImporter.scene(sceneID);
-    auto sceneChildren = sceneData.children3D();
-    logger.Info("Parsing scene : [%s] w/ [%d] children", sceneName.c_str(),
-                sceneChildren.size());
 
+    auto sceneChildren = sceneData.children3D();
     for (auto parentNode : sceneChildren) {
       BuildChildTree("", parentNode);
     }
+
+    auto meshCount = gltfImporter.meshCount();
+    logger.Info("Parsing GLTF scene : [%s] w/ [%d] nodes", sceneName.c_str(),
+                meshCount);
+    for (uint32_t meshID = 0; meshID < meshCount; meshID++) {
+      const std::string meshName = gltfImporter.meshName(meshID);
+
+      logger.Info("Compiling mesh : %s", meshName.c_str());
+
+      meshNames.push_back(meshName);
+      Corrade::Containers::Optional<Magnum::Trade::MeshData> meshData =
+          gltfImporter.mesh(meshID);
+
+      compiledMeshes.push_back(
+          Magnum::MeshTools::compile(*meshData));
+      nodeCount++;
+    }
   }
-
-  // auto meshCount = gltfImporter.mesh3DCount();
-  // for (uint32_t meshID = 0; meshID < meshCount; meshID++) {
-  //   const std::string meshName = gltfImporter.mesh3DName(meshID);
-  //   logger.Info("Compiling mesh : %s", meshName.c_str());
-  //   meshNames.push_back(meshName);
-  //   // Corrade::Containers::Optional<Magnum::Trade::MeshData3D> meshData =
-  //   //         gltfImporter.mesh3D(meshID);
-
-  //   compiledMeshes.push_back(
-  //       Magnum::MeshTools::compile(*gltfImporter.mesh3D(meshID)));
-  // }
 
   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
                       std::chrono::system_clock::now() - start)
